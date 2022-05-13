9737f258aed7eef2d4949923ce1e258331654fe7
Joysy-Joji <Joysyk97@gmail.com>
2022-05-13 18:30:07 +0530

    commmit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+    <style>
+        .btn{
+            background-color: darkred;
+            border: none;
+            color: white;
+            padding: 15px 32px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 16px;
+            margin: 4px 2px;
+            cursor: pointer;
+        }
+    </style>
+</head>
+
+<body>
+@if(!empty($status = session('status')))
+    <span style="color: green;">{{ $status }}</span>
+@endif
+<h2 style="text-align: center; font-family: 'Bookman Old Style'; font-size: xx-large; color: black">Welcome {{ $name }}</h2>
+<h1 style="color: #005cbf">Name : {{ Auth()->user()->name}}</h1>
+<h1 style="color: #005cbf">Email : {{ Auth()->user()->email}}</h1>
+<a href="{{URL::to('logout')}}" class="btn">LOGOUT</a>
+
+</body>
+</body>
+</html>
