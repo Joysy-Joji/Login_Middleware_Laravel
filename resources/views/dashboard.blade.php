efa0644a859e3dda00ed14cdbfe88c5bec039b26
Joysy-Joji <Joysyk97@gmail.com>
2022-05-30 23:37:14 +0530

    commit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,7 +13,7 @@
             <td><a  href="{{ URL::to('logout') }}" class="btn">LOGOUT</a></td>
         </tr>
     </table>
-    <table  class="table table-white mt-5" border="1" style="alignment: right">
+    <table  class="table table-white mt-5" border="1" id="logintable" style="alignment: right">
         <thead>
         <tr>
 
@@ -45,6 +45,27 @@
 
     {{ $user_logins->links() }}
 </div>
+    @section('scripts')
+        <script src="https://code.jquery.com/jquery-3.6.0.js"
+                integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
+                crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+        <script type="text/javascript" src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
+        <script type="text/javascript">
+
+            $(document).ready(function() {
+                $('#logintable').DataTable( {
+                {{--"ajax": "{{ route('web.dashboard') }}",--}}
+                    // data:{
+                    //
+                    // }
+                });
+            });
+        </script>
+
+
+    @endsection
     </body>
 @endsection
 
